9dac5e0d169cdb45079c3019f13bf9b4e6e907d4
BahadirDogrusoz <dogrusozba@gmail.com>
2020-09-24 13:59:57 +0300

    Operators

---
+++ main.py
@@ -0,0 +1,16 @@
+sayi1 = 35
+sayi2 = 65
+
+print(sayi1 + sayi2)
+print(sayi1 - sayi2)
+print(sayi1 * sayi2)
+print(sayi1 / sayi2)
+
+print(sayi1 > sayi2)
+print(sayi1 < sayi2)
+
+sayi1 += 1
+print(sayi1)
+
+sayi2 -= 1
+print(sayi2)
